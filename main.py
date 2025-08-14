919767c66be79601e36375c0e57a542c474c9a63
atommnava <atommunoznava@gmail.com>
2025-08-14 17:59:40 -0600

    Query parametros

--- main.py
+++ main.py
@@ -15,3 +15,10 @@ inventory = {
 def get_item(item_id: int = Path(description="The ID of the Item you want to view.")):
     return inventory[item_id]
 
+@app.get("/get-by-name")
+def get_by_name(name: Optional[str] = None, test: int):
+    for item_id in inventory:
+        if inventory[item_id]["name"] == name:
+            return inventory[item_id]
+        return {"Data" : "Not found"}
+
