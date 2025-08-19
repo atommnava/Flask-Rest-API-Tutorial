656ce80f750061a9f5e9b0c2dba30b2aac415bc3
atommnava <atommunoznava@gmail.com>
2025-08-19 15:53:34 -0600

    Asterisco, aceptar todos los parametros posibles

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, Path
 
-
 app = FastAPI()
 
 inventory = {
@@ -11,12 +10,12 @@ inventory = {
     }
 }
 
-@app.get("/get-item/{item_id}/{name}")
+@app.get("/get-item/{item_id}")
 def get_item(item_id: int = Path(description="The ID of the Item you want to view.")):
     return inventory[item_id]
 
 @app.get("/get-by-name")
-def get_by_name(name: Optional[str] = None, test: int):
+def get_by_name(*, name: Optional[str] = None, test: int):
     for item_id in inventory:
         if inventory[item_id]["name"] == name:
             return inventory[item_id]
